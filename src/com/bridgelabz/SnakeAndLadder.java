fd0366e60138efdce96cdaf508fb7acd59faec2f
Pranali-22 <lpranali22@gmail.com>
2022-09-07 19:59:01 +0530

    Added: UC1: Starting position

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author Dell
+ *
+ */
+public class SnakeAndLadder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int position=0;	
+		
+		System.out.println("Starting position is "+position);
+
+	}
+
+}
