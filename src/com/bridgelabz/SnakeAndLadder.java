6a952a79d28d338547550d69758523873c84f4c8
Pranali-22 <lpranali22@gmail.com>
2022-09-07 21:06:42 +0530

    Added: UC3: Checks for a Option(no play, ladder, snake)

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -22,7 +22,37 @@ public class SnakeAndLadder {
 		Random r=new Random();
 		int roll=r.nextInt(6)+1;
 		System.out.println("number on die : "+roll);
+		
+		//code to check for option
+		int option=r.nextInt(3);
+		switch (option) {
+			case 0:
+				System.out.println("No Play");
+				break;
+				
+			case 1:
+				System.out.println("Got ladder");
+				if(position+roll <=100) {
+					position = position+roll;
+				}
+				break;
+				
+			case 2:
+				System.out.println("Got snake");
+				if(position!=0 && position-roll>=0 ) {
+					position = position-roll;
+				}
+				else {
+					position=0;
+				}
+				break;
+				
+			default :
+				System.out.println("Incorrect  option");
+				break;
+		}
 
+		System.out.println("current position is "+position);
 	}
 
 }
