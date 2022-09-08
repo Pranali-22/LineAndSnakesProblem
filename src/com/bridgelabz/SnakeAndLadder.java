4bb0999f37151db721b609a939229f607cf7e9cc
Pranali-22 <lpranali22@gmail.com>
2022-09-08 20:30:12 +0530

    Added: UC6: Print number of times dice played & position after each dice

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -25,39 +25,43 @@ public class SnakeAndLadder {
 		//System.out.println("number on die : "+roll);
 		
 		//code to check for option
+		int count=0;
 		while(position<100) {
-		roll=r.nextInt(6)+1;
-		System.out.println("number on die : "+roll);
-		int option=r.nextInt(3);
-		switch (option) {
-			case 0:
-				System.out.println("No Play");
-				break;
-				
-			case 1:
-				System.out.println("Got ladder");
-				if(position+roll <=100) {
-					position = position+roll;
-				}
-				break;
-				
-			case 2:
-				System.out.println("Got snake");
-				if(position!=0 && position-roll>=0 ) {
-					position = position-roll;
-				}
-				else {
-					position=0;
-				}
-				break;
-				
-			default :
-				System.out.println("Incorrect  option");
-				break;
+			roll=r.nextInt(6)+1;
+			System.out.println("number on die : "+roll);
+			int option=r.nextInt(3);
+			count++;
+			switch (option) {
+				case 0:
+					System.out.println("No Play");
+					break;
+					
+				case 1:
+					System.out.println("Got ladder");
+					if(position+roll <=100) {
+						position = position+roll;
+					}
+					break;
+					
+				case 2:
+					System.out.println("Got snake");
+					if(position!=0 && position-roll>=0 ) {
+						position = position-roll;
+					}
+					else {
+						position=0;
+					}
+					break;
+					
+				default :
+					System.out.println("Incorrect  option");
+					break;
+			}
+			System.out.println("current position is "+position);
 		}
-
-		}
-		System.out.println("current position is "+position);
+		
+		System.out.println("Number of times dice was played = "+count);
+		
 	}
 
 }
